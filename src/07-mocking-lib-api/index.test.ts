2b5538dd04efece68c06a255328a1ee486fdab16
Denis Filatov <denis.filatov@ideus.biz>
2024-02-12 22:23:04 +0200

    feat: implement 07-mocking-lib-api

--- src/07-mocking-lib-api/index.test.ts
+++ src/07-mocking-lib-api/index.test.ts
@@ -1,17 +1,28 @@
 // Uncomment the code below and write your tests
-/* import axios from 'axios';
-import { throttledGetDataFromApi } from './index'; */
+import axios from 'axios';
+import { throttledGetDataFromApi } from './index';
 
 describe('throttledGetDataFromApi', () => {
   test('should create instance with provided base url', async () => {
-    // Write your test here
+    const relativePath = '/posts/1';
+    const getDataSpy = jest.spyOn(axios, 'create');
+    await throttledGetDataFromApi(relativePath);
+    expect(getDataSpy).toHaveBeenCalledWith({
+      baseURL: 'https://jsonplaceholder.typicode.com',
+    });
   });
 
   test('should perform request to correct provided url', async () => {
-    // Write your test here
+    const relativePath = '/posts';
+    const getSpy = jest.spyOn(axios, 'get');
+    await throttledGetDataFromApi(relativePath);
+    expect(getSpy).toEqual(expect.anything());
   });
 
+
   test('should return response data', async () => {
-    // Write your test here
+    const relativePath = '/posts/1';
+    const responseData = await throttledGetDataFromApi(relativePath);
+    expect(responseData).toEqual(expect.anything());
   });
 });
